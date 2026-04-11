330e97381b0e0bcb797a6716ac9bf152cbef9bd4
Allen Liu <allenliuzihao@gmail.com>
2026-04-10 22:49:29 -0700

    sparse vector multiplication

--- Vector.h
+++ Vector.h
@@ -3,28 +3,9 @@
 template <FloatOrInt T, size_t N>
 class Vector
 {
-protected:
-    Vector()
-    {
-        m_data.fill(T{});  // Initialize all elements to the default value of T
-    }
-
-    Vector(const std::array<T, N>& data)
-    {
-        m_data = data;  // Copy the input data to the member variable
-    }
-
-
-    std::array<T, N> m_data;  // Array of N elements of type T
 public:
     virtual float Dot(const Vector<T, N>& other) const = 0;
-
-    // Protected getter to access elements
-    const T& operator[](size_t index) const
-    {
-        assert(index < m_data.size());
-        return m_data[index];
-    }
+    virtual const T& operator[](size_t index) const = 0;
 };
 
 template <FloatOrInt T, size_t N>
@@ -32,13 +13,22 @@ class ScalarVector : public Vector<T, N>
 {
 public:
     // Default constructor
-    ScalarVector() : Vector<T, N>()
+    ScalarVector()
     {
+        m_data.fill(T{});  // Initialize all elements to the default value of T
     }
 
     // Constructor accepting array data
-    explicit ScalarVector(const std::array<T, N>& data) : Vector<T, N>(data)
+    explicit ScalarVector(const std::array<T, N>& data)
     {
+        m_data = data;  // Copy the input data to the member variable
+    }
+
+    // Protected getter to access elements
+    const T& operator[](size_t index) const override
+    {
+        assert(index < m_data.size());
+        return m_data[index];
     }
 
     float Dot(const Vector<T, N>& other) const override
@@ -51,4 +41,65 @@ public:
         }
         return result;
     }
+private:
+    std::array<T, N> m_data;  // Array of N elements of type T
+};
+
+template <FloatOrInt T, size_t N>
+class SparseVector : public Vector<T, N>
+{
+public:
+    // Constructor accepting array data
+    explicit SparseVector(const std::array<T, N>& data) 
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            if (data[i] != T{}) // Only store non-zero elements
+            {
+                m_indices.push_back(i);
+                m_data.push_back(data[i]);
+            }
+        }
+    }
+
+    float Dot(const Vector<T, N>& other) const override
+    {
+        float result = 0.0f;
+
+        uint32_t i = 0;
+        for (size_t index : m_indices)
+        {
+            // Perform the dot product calculation
+            result += this->m_data[i++] * other[index];
+        }
+        return result;
+    }
+
+    // Protected getter to access elements
+    const T& operator[](size_t index) const override
+    {
+        // binary search on m_indices to find the index
+        int left = 0, right = m_indices.size() - 1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (m_indices[mid] == index)
+            {
+                return m_data[mid];
+            }
+            else if (m_indices[mid] < index)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return T{}; // Return default value if
+    }
+
+private:
+    std::vector<size_t> m_indices;
+    std::vector<T> m_data;  
 };
--- pch.h
+++ pch.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <vector>
 #include <array>
 #include <concepts>
 
